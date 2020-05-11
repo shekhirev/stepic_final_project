471bfb3c569bf9b5f15234cd1b7e571001d63556
Sergey Shekhirev <shekhirev@radario.co>
2020-05-11 16:00:39 +0300

    рефакторинг для main page

--- test_main_page.py
+++ test_main_page.py
@@ -3,8 +3,8 @@ from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
+    main_page = MainPage(browser, link)
+    main_page.open()
+    main_page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
