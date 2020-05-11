9be53e1fe9c069e86fc247465d6185a53ea25059
Sergey Shekhirev <shekhirev@radario.co>
2020-05-11 17:34:57 +0300

    разделены методы проверки цены и названия

--- pages/product_page.py
+++ pages/product_page.py
@@ -10,13 +10,19 @@ class ProductPage(BasePage):
         self.check_busket_price()
         
     def check_busket_price(self):
-        price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
-        busket_price = self.browser.find_element(*ProductPageLocators.BUSKET_PRODUCT_PRICE).text
+        price = self.get_product_price(*ProductPageLocators.PRODUCT_PRICE)
+        busket_price = self.get_product_price(*ProductPageLocators.BUSKET_PRODUCT_PRICE)        
         assert price == busket_price, "Цена в корзине не совпадает с ценой добавленного товара"
         print(busket_price)
 
     def check_busket_product_name(self):
-        name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
-        busket_name = self.browser.find_element(*ProductPageLocators.BUSKET_PRODUCT_NAME).text
+        name = self.get_product_name(*ProductPageLocators.PRODUCT_NAME)
+        busket_name = self.get_product_name(*ProductPageLocators.BUSKET_PRODUCT_NAME)
         assert name == busket_name, "Название товара в корзине не совпадает с добавленным"
         print(busket_name)
+
+    def get_product_price(self, *price):
+        return self.browser.find_element(*price).text
+    
+    def get_product_name(self, *name):
+        return self.browser.find_element(*name).text
