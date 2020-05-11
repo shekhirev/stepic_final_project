7474b59b921d0aff0526c3058ba88f8bd27668e5
Sergey Shekhirev <shekhirev@radario.co>
2020-05-11 20:32:20 +0300

    добавлена группировка для main page

--- test_main_page.py
+++ test_main_page.py
@@ -5,13 +5,21 @@ from .pages.busket_page import BusketPage
 from .pages.locators import MainPageLocators
 from .pages.locators import ProductPageLocators
 
-@pytest.mark.skip
-def test_guest_can_go_to_login_page(browser):
-    main_page = MainPage(browser, MainPageLocators.BASIC_MAIN_PAGE_LINK)
-    main_page.open()
-    main_page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        main_page = MainPage(browser, MainPageLocators.BASIC_MAIN_PAGE_LINK)
+        main_page.open()
+        main_page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        main_page = MainPage(browser, MainPageLocators.BASIC_MAIN_PAGE_LINK)
+        main_page.open()
+        main_page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_link()
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     main_page = MainPage(browser, MainPageLocators.BASIC_MAIN_PAGE_LINK)
